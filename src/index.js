35aba1a70ef1f7e5ae0ac0a1b8e10635ff5287d0
Kirill Gladkov <crosfire2011@gmail.com>
2019-06-25 09:28:57 +0300

    implementing ws connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { setContext } from 'apollo-link-context'
+import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 import './styles/index.css';
 import App from './components/App';
@@ -30,9 +33,28 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
