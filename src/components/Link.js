e2b5c670404bfea20b16721fb695ba1bf2f86b7b
Kirill Gladkov <crosfire2011@gmail.com>
2019-06-25 09:25:05 +0300

    add votes to link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag';
 
-const Link = ({ link }) => {
+import { AUTH_TOKEN } from './../constants';
+import { timeDifferenceForDate } from '../utils';
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const Link = ({ link, index, updateStoreAfterVote }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
   return (
-    <div>
-      {link.description} ({link.url}) createdAt: {link.createdAt}
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <Mutation mutation={VOTE_MUTATION} 
+                    variables={{ linkId: link.id }} 
+                    update={(store, { data: { vote } }) => updateStoreAfterVote(store, vote, link.id)
+          }>
+            {voteMutation => (
+              <div className="ml1 gray f11 pointer" onClick={voteMutation}>▲</div>
+            )}
+          </Mutation>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{' '}
+          {link.postedBy
+            ? link.postedBy.name
+            : 'Unknown'}{' '}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
+      </div>
     </div>
   )
 }
