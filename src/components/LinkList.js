348183bb4382232e8b921c4db27319a625f6716a
Kirill Gladkov <crosfire2011@gmail.com>
2019-06-25 09:37:09 +0300

    implementing subscription to a new link add

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -26,6 +26,27 @@ export const FEED_QUERY = gql`
     }
   }`;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+subscription {
+  newLink {
+    id
+    url
+    description
+    createdAt
+    postedBy {
+      id
+      name
+    }
+    votes {
+      id
+      user {
+        id
+      }
+    }
+  }
+}
+`;
+
 const LinkList = () => {
 
   const _updateCacheAfterVote = (store, createVote, linkId) => {
@@ -37,18 +58,41 @@ const LinkList = () => {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
+  const _subscribeToNewLinks = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
   return (
     <Query query={FEED_QUERY}>
       {
-        ({ loading, error, data }) => {
+        ({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div style={{ margin: 'auto', color: '#ccc' }}>Fetching...</div>
           if (error) return <div style={{ margin: 'auto', color: 'red' }}>Error :(</div>
 
-          const linksToRender = data.feed.links
+          _subscribeToNewLinks(subscribeToMore);
+          const linksToRender = data.feed.links;
 
           return (
             <div>
-              {linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} updateStoreAfterVote={_updateCacheAfterVote}/>)}
+              {linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} updateStoreAfterVote={_updateCacheAfterVote} />)}
             </div>
           )
         }
