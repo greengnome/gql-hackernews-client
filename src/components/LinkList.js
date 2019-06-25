e9e494bd4a1c0b1ae9b7b0d10bfb669c24bb4228
Kirill Gladkov <crosfire2011@gmail.com>
2019-06-25 10:05:48 +0300

    implementing pagination

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag';
 
 import Link from './Link';
+import { LINKS_PER_PAGE } from '../constants';
 
 export const FEED_QUERY = gql`
-  {
-    feed {
+  query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(first: $first, skip: $skip, orderBy: $orderBy) {
       links {
         id
         createdAt
@@ -23,8 +24,10 @@ export const FEED_QUERY = gql`
           }
         }
       }
+      count
     }
-  }`;
+  }
+`;
 
 const NEW_LINKS_SUBSCRIPTION = gql`
 subscription {
@@ -74,14 +77,22 @@ const NEW_VOTES_SUBSCRIPTION = gql`
   }
 `;
 
-const LinkList = () => {
+const LinkList = ({ location, match, history }) => {
 
   const _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: FEED_QUERY })
+    const isNewPage = location.pathname.includes('new')
+    const page = parseInt(match.params.page, 10)
+
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+    const first = isNewPage ? LINKS_PER_PAGE : 100
+    const orderBy = isNewPage ? 'createdAt_DESC' : null
+    const data = store.readQuery({
+      query: FEED_QUERY,
+      variables: { first, skip, orderBy }
+    })
 
     const votedLink = data.feed.links.find(link => link.id === linkId)
     votedLink.votes = createVote.link.votes
-
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
@@ -113,8 +124,45 @@ const LinkList = () => {
     })
   }
 
+  const _getQueryVariables = () => {
+    const isNewPage = location.pathname.includes('new')
+    const page = parseInt(match.params.page, 10)
+
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+    const first = isNewPage ? LINKS_PER_PAGE : 100
+    const orderBy = isNewPage ? 'createdAt_DESC' : null
+    return { first, skip, orderBy }
+  }
+
+  const _getLinksToRender = data => {
+    const isNewPage = location.pathname.includes('new')
+    if (isNewPage) {
+      return data.feed.links
+    }
+    const rankedLinks = data.feed.links.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+
+    return rankedLinks;
+  }
+
+  const _nextPage = data => {
+    const page = parseInt(match.params.page, 10)
+    if (page <= data.feed.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1
+      history.push(`/new/${nextPage}`)
+    }
+  }
+
+  const _previousPage = () => {
+    const page = parseInt(match.params.page, 10)
+    if (page > 1) {
+      const previousPage = page - 1
+      history.push(`/new/${previousPage}`)
+    }
+  }
+
   return (
-    <Query query={FEED_QUERY}>
+    <Query query={FEED_QUERY} variables={_getQueryVariables()}>
       {
         ({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div style={{ margin: 'auto', color: '#ccc' }}>Fetching...</div>
@@ -123,12 +171,30 @@ const LinkList = () => {
           _subscribeToNewLinks(subscribeToMore);
           _subscribeToNewVotes(subscribeToMore);
 
-          const linksToRender = data.feed.links;
+          const linksToRender = _getLinksToRender(data)
+          const isNewPage = location.pathname.includes('new')
+          const pageIndex = match.params.page
+            ? (match.params.page - 1) * LINKS_PER_PAGE
+            : 0
 
           return (
-            <div>
-              {linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} updateStoreAfterVote={_updateCacheAfterVote} />)}
-            </div>
+            <>
+              {
+                linksToRender.map((link, index) => <Link key={link.id}
+                  link={link}
+                  index={index + pageIndex}
+                  updateStoreAfterVote={_updateCacheAfterVote} />)
+              }
+              {
+                isNewPage && (
+                  <div className="flex ml4 mv3 gray">
+                    <div className="pointer mr2" onClick={_previousPage}>Previous
+                  </div>
+                    <div className="pointer" onClick={() => _nextPage(data)}>Next</div>
+                  </div>
+                )
+              }
+            </>
           )
         }
       }
@@ -136,4 +202,4 @@ const LinkList = () => {
   )
 }
 
-export default LinkList
+export default LinkList;
